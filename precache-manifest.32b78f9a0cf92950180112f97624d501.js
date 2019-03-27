ac453254dd053de19e122adcc33c3d8d9035174f
Bartosz Łągwa <bartosz.lagwa@neurosoft.pl>
2019-03-27 14:37:18 +0100

    Updates

---
+++ precache-manifest.32b78f9a0cf92950180112f97624d501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "660d417969be2c794668",
+    "url": "/urodzinowy-pomocnik/static/js/runtime~main.db808dce.js"
+  },
+  {
+    "revision": "ab8ff659e9e9de94fecd",
+    "url": "/urodzinowy-pomocnik/static/js/main.5e5a270b.chunk.js"
+  },
+  {
+    "revision": "d12900f7dafd68cdb958",
+    "url": "/urodzinowy-pomocnik/static/js/2.1577f0aa.chunk.js"
+  },
+  {
+    "revision": "ab8ff659e9e9de94fecd",
+    "url": "/urodzinowy-pomocnik/static/css/main.babc6af0.chunk.css"
+  },
+  {
+    "revision": "d12900f7dafd68cdb958",
+    "url": "/urodzinowy-pomocnik/static/css/2.01ffda2a.chunk.css"
+  },
+  {
+    "revision": "d8e0a5229985d9801250575f7455339e",
+    "url": "/urodzinowy-pomocnik/index.html"
+  }
+];
